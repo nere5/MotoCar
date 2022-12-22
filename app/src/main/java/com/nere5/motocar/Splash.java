f2d392929fd0030e7653084c30b7799f2b6040e3
Nelson Rios <nere5@hotmail.com>
2022-12-22 18:05:41 -0500

    tercer commit Actividad Inicio y splash

--- app/src/main/java/com/nere5/motocar/Splash.java
+++ app/src/main/java/com/nere5/motocar/Splash.java
@@ -2,13 +2,30 @@ package com.nere5.motocar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask splash = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Inicio.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(splash, 3000);
     }
+
 }
